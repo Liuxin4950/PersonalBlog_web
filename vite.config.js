1e07f85594963bfa97483e3f602f4fec3afb733c
liuxin <1371149587@qq.com>
2025-01-01 01:13:16 +0800

    重新拉取安装了vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
-  base: '/',
   plugins: [vue(),
   AutoImport({
     resolvers: [ElementPlusResolver()],
